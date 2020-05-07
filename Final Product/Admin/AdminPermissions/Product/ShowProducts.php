31ff652addccdf776b20918fa0f454e875a91f04
Elliotsdreed <elliotsdreed@icloud.com>
2020-05-07 17:41:28 +0100

    New icon navbar on all pages except system reports

--- Final Product/Admin/AdminPermissions/Product/ShowProducts.php
+++ Final Product/Admin/AdminPermissions/Product/ShowProducts.php
@@ -5,20 +5,29 @@
     header("Location:adminlogin.php") ;
     }
     ?>
+
 <head>
-<title>G4U</title>
-  <link href="style.css" rel="stylesheet" type="text/css">
-  <link href="Admin.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
-  </head>
-<body style="background-color:#a6b2c1">
-  <div class="topnav" ALIGN="center">
-    <img src="../../../g4uimageprototype.png" alt="G4ULogo" width="12.5%"></img>
-    <input type="text" placeholder="Search.." style="margin-top: 100px;">
-    <button id="search-button" class="btn btn-success">Search!</button>
-    <button id="basket-button" class="btn btn-warning">Basket</button>
-    <button id="logout-button" class="btn btn-danger">Log Out!</button>
-  </div>
+  <title>G4U</title>
+  <link rel="stylesheet" href="../../../style.css" type="text/css">
+  <link rel="stylesheet" href="../../../website.css" type="text/css">
+  <link rel="stylesheet" href="Admin.css"  type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:#AEB9C7">
+    <div style="background-color:#a6b2c1;" class="topnav" align="center">
+        
+        <img src="../../../Back.png" id="back" alt="back" style=width:50%; height="50%"></img>
+        <img src="../../../g4uimageprototype.png" id="g4u-logo" alt="G4ULogo"></img>
+        <div class="search-box" id="search-bar">
+            <input type="text" autocomplete="on" placeholder="Search product..." />
+        <div class="result"></div>
+        </div>
+       
+       <img src="../../../LogoutButton.png" id="logout-button" alt="logout-button" onclick="window.location.href = '../logout.php'" ></img>
+       </div>
 
 
 <div class="outputresults">
--- Final Product/Admin/AdminPermissions/User/addauser.php
+++ Final Product/Admin/AdminPermissions/User/addauser.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>G4UItems</title>
-    <link href="../../../style.css" 
-          rel="stylesheet" 
-          type="text/css">
-          <link href="../../../../style.css" 
-          rel="stylesheet" 
-          type="text/css">
-    <link href="../../../ItemsPage.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-            crossorigin="anonymous">
-            
+  <title>G4U</title>
+  <link rel="stylesheet" href="../../../style.css" type="text/css">
+  <link rel="stylesheet" href="../../../website.css" type="text/css">
+  <link rel="stylesheet" href="Admin.css"  type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
-<body style="background-color:#a6b2c1">
-    <div class="topnav" align="center">
-        <button id="back-button" class="btn btn-danger">Back</button>
+<body style="background-color:#AEB9C7">
+    <div style="background-color:#a6b2c1;" class="topnav" align="center">
+        
+        <img src="../../../Back.png" id="back" alt="back" style=width:50%; height="50%"></img>
         <img src="../../../g4uimageprototype.png" id="g4u-logo" alt="G4ULogo"></img>
         <div class="search-box" id="search-bar">
             <input type="text" autocomplete="on" placeholder="Search product..." />
         <div class="result"></div>
         </div>
-        <button id="search-button" class="btn btn-success">Search</button>
-        <button id="basket-button" class="btn btn-warning">Basket</button>
-        <button id="logout-button" class="btn btn-danger">Log Out</button>
-    </div>
+       
+       <img src="../../../LogoutButton.png" id="logout-button" alt="logout-button" onclick="window.location.href = '../logout.php'" ></img>
+       </div>
     <br><br>
 </body>
 </html>
